525ff3072684a6183cca880eb18600a4e407d48f
Rayan <renomohamed16@gmail.com>
2025-01-23 12:20:18 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,60 @@
+// let person = {
+//     firstName: 'rayan',
+//     lastName : 'mohamed',
+//     age: 22,
+//     marrign:true 
+
+// };
+// console.table(person);
+// console.log(person);
+// console.log(person.firstName)
+// document.write(person.firstName+ ' '+person.lastName)
+
+// let firstName = 'rayan',
+//      lastName = 'moahmed';
+//      console.log(firstName.length);
+//      console.log(firstName.toLocaleUpperCase());
+//      console.log(firstName.concat(' ' +lastName));
+//      console.log(firstName.indexOf('y'));
+//      console.log(firstName.slice(0,3));
+//      console.log(firstName.slice(3,4));
+//      console.log(firstName.slice(-3));
+
+
+// let x = 'hello my name is rayan';
+// let y = x.split(' ');
+// console.log(y[2]);
+
+
+
+// let x = '   rayan';
+// console.log (x.trim());
+
+// let x = 'Hello welcome to my home'
+// console.log(x.repeat('home' ,'class'));
+
+     
+
+
+// let x = 'rayan';
+// console.log (x.trim());
+
+
+
+// let fruits = ['apple' ,'mango','watermelon'];
+// fruits.unshift('cherry')
+// fruits.push('banana' ,'kiwi');
+// fruits.splice(2,0,'orange')
+// console.log(fruits);
+// console.log(fruits.pop)
+
+
+
+// let toDay = new Date();
+// console.log(toDay.getDay());
+// console.log(toDay.getDate());
+// console.log(toDay.getFullYear());
+// console.log(toDay.getMonth());
+
+
+
